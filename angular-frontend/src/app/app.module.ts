8a41e230a88a76b57cb6a69cdaa24e51eb64e7ae
ANUJ-POHANKAR <POHANKARANUJ@GMAIL.COM>
2025-01-25 22:10:45 +0530

    Update app.module.ts

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; // This is crucial for ngForm
 import { HttpClientModule } from '@angular/common/http';
 
 import { FormComponent } from './form/form.component'; // Import the Form component
@@ -12,7 +12,7 @@ import { OrderService } from './services/order.service'; // Import the Order ser
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule, // Import FormsModule here
     HttpClientModule, // Import HttpClientModule to use HTTP services
   ],
   providers: [
