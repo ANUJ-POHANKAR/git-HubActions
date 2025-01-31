a877cc900556efaa0d7db826b7b59401cc5d1534
ANUJ-POHANKAR <POHANKARANUJ@GMAIL.COM>
2025-01-31 11:56:06 +0530

    Delete angular-frontend directory

--- angular-frontend/src/app/models/order.ts
+++
@@ -1,8 +0,0 @@
-export interface Order {
-    cusname: string;
-    telephone: string;
-    mail: string;
-    book: string;
-    quantity: number;
-}
-
--- angular-frontend/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';  // This assumes you have an AppModule in the src/app directory.
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
